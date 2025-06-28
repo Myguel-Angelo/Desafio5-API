bd3f3902a4443c5d19ccee8fd4cffbd06fe670c7
Myguel-Angelo <angelo.nunes576@gmail.com>
2025-06-28 11:24:42 -0300

    feat: atualização para mysql

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
+require('dotenv').config()
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 const usuarioRoutes = require('./routes/usuarios');
+const medicoRoutes = require('./routes/medicos');
+const areasRoutes = require('./routes/areas');
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('API rodando! Use /usuarios para acessar os usuários.');
 });
 
-app.use('/usuarios', usuarioRoutes);
+app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/medicos', medicoRoutes);
+app.use('/api/areas', areasRoutes);
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
